05e7c521225fb5d5c5a607dcc4aa60b2b45dc701
Mikiyoarada-cmyk <mikiyoarada@gmail.com>
2026-06-18 05:33:52 -0700

    play bingo button

--- bot.js
+++ bot.js
@@ -6,41 +6,36 @@ const TelegramBot = require("node-telegram-bot-api");
 const app = express();
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, {
-  polling: true
+polling: true
 });
 
 const GAME_URL = "https://ethiobingo-1j5k.onrender.com/game.html";
 
-/* START */
-bot.onText(/\/start/, (msg) => {
-
-  bot.sendMessage(
-    msg.chat.id,
-    "🎯 ETHIO BINGO\n\nClick PLAY BINGO",
-    {
-      reply_markup: {
-        inline_keyboard: [
-          [
-            {
-              text: "🎮 PLAY BINGO",
-              url: GAME_URL
-            }
-          ]
-        ]
-      }
-    }
-  );
-
+bot.onText(//start/, (msg) => {
+bot.sendMessage(
+msg.chat.id,
+"🎯 ETHIO BINGO",
+{
+reply_markup: {
+inline_keyboard: [
+[
+{
+text: "🎮 PLAY BINGO",
+url: GAME_URL
+}
+]
+]
+}
+}
+);
 });
 
-/* HOME PAGE */
 app.get("/", (req, res) => {
-  res.send("Ethio Bingo Running");
+res.send("Ethio Bingo Running");
 });
 
-/* START SERVER */
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running");
+console.log("Server running");
 });
