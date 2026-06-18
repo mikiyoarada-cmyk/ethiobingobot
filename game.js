7dd0fe3871a078b83f42569fa21213125724b260
Mikiyoarada-cmyk <mikiyoarada@gmail.com>
2026-06-18 07:22:52 -0700

    fix bingo auto marking and voice calling

--- game.js
+++ game.js
@@ -1,289 +1,287 @@
 const { Server } = require("socket.io");
 
+
 let io;
 
 let cards = [];
+
 let calledNumbers = [];
 
-let gameInterval;
-let countdown = 30;
-
-
-
-/* CREATE REAL BINGO 5x5 CARD */
-function createCard(id){
-
-    let B = [];
-    let I = [];
-    let N = [];
-    let G = [];
-    let O = [];
-
-
-    while(B.length < 5){
-        let n = random(1,15);
-        if(!B.includes(n)) B.push(n);
-    }
-
-
-    while(I.length < 5){
-        let n = random(16,30);
-        if(!I.includes(n)) I.push(n);
-    }
-
-
-    while(N.length < 5){
-        let n = random(31,45);
-        if(!N.includes(n)) N.push(n);
-    }
-
-
-    while(G.length < 5){
-        let n = random(46,60);
-        if(!G.includes(n)) G.push(n);
-    }
-
-
-    while(O.length < 5){
-        let n = random(61,75);
-        if(!O.includes(n)) O.push(n);
-    }
-
-
-    return {
-
-        id:id,
-
-        numbers:[
-
-            B[0],I[0],N[0],G[0],O[0],
-            B[1],I[1],N[1],G[1],O[1],
-            B[2],I[2],"FREE",G[2],O[2],
-            B[3],I[3],N[3],G[3],O[3],
-            B[4],I[4],N[4],G[4],O[4]
-
-        ]
-
-    };
-
-}
+let gameTimer;
 
 
 
 function random(min,max){
 
- return Math.floor(Math.random()*(max-min+1))+min;
+return Math.floor(Math.random()*(max-min+1))+min;
 
 }
 
 
 
-/* CREATE 600 UNIQUE CARDS */
-function createCards(){
 
- cards=[];
+function makeColumn(min,max){
 
- let saved=[];
+let arr=[];
 
 
- while(cards.length < 600){
+while(arr.length<5){
 
-    let card=createCard(cards.length+1);
+let n=random(min,max);
 
+if(!arr.includes(n)){
 
-    let key=JSON.stringify(card.numbers);
+arr.push(n);
 
+}
 
-    if(!saved.includes(key)){
+}
 
-        saved.push(key);
-
-        cards.push(card);
-
-    }
-
- }
+return arr;
 
 }
 
 
 
-/* COUNTDOWN */
-function startCountdown(){
 
- countdown=30;
+function createCard(id){
 
 
- let timer=setInterval(()=>{
+let B=makeColumn(1,15);
+
+let I=makeColumn(16,30);
+
+let N=makeColumn(31,45);
+
+let G=makeColumn(46,60);
+
+let O=makeColumn(61,75);
 
 
-    io.emit(
-        "countdown",
-        {
-            countdown:countdown
-        }
-    );
 
+return {
 
-    countdown--;
+id:id,
 
+numbers:[
 
-    if(countdown < 0){
+B[0],I[0],N[0],G[0],O[0],
+B[1],I[1],N[1],G[1],O[1],
+B[2],I[2],"FREE",G[2],O[2],
+B[3],I[3],N[3],G[3],O[3],
+B[4],I[4],N[4],G[4],O[4]
 
-        clearInterval(timer);
+]
 
-        startGame();
+};
 
-    }
-
-
- },1000);
 
 }
 
 
 
-/* START GAME */
+
+
+function create600Cards(){
+
+
+cards=[];
+
+
+for(let i=1;i<=600;i++){
+
+cards.push(createCard(i));
+
+}
+
+
+}
+
+
+
+
+
+
 function startGame(){
 
- calledNumbers=[];
+
+calledNumbers=[];
 
 
- io.emit("gameStart");
+io.emit(
+"gameStart"
+);
 
 
- gameInterval=setInterval(()=>{
 
-    callNumber();
+gameTimer=setInterval(()=>{
 
- },3000);
+
+if(calledNumbers.length>=75){
+
+clearInterval(gameTimer);
+
+return;
+
+}
+
+
+
+let number;
+
+
+do{
+
+number=random(1,75);
+
+}
+while(calledNumbers.includes(number));
+
+
+
+calledNumbers.push(number);
+
+
+
+io.emit(
+"number",
+{
+number:number,
+called:calledNumbers
+}
+);
+
+
+
+},2000);
+
 
 
 }
 
 
 
-/* CALL NUMBERS */
-function callNumber(){
-
-
- if(calledNumbers.length>=75){
-
-    endGame();
-
-    return;
-
- }
-
-
- let n;
-
-
- do{
-
-    n=random(1,75);
-
-
- }while(calledNumbers.includes(n));
 
 
 
- calledNumbers.push(n);
+
+
+function startCountdown(){
+
+
+let count=30;
 
 
 
- io.emit(
-    "number",
-    {
-        number:n
-    }
- );
+let timer=setInterval(()=>{
+
+
+io.emit(
+"countdown",
+{
+countdown:count
+}
+);
+
+
+
+count--;
+
+
+
+if(count<0){
+
+clearInterval(timer);
+
+startGame();
+
+}
+
+
+
+},1000);
 
 
 }
 
 
 
-/* END */
-function endGame(){
-
- clearInterval(gameInterval);
-
-
- io.emit("gameEnd");
-
-
- setTimeout(()=>{
-
-    createCards();
-
-    startCountdown();
-
- },5000);
-
-
-}
 
 
 
-/* SERVER */
 function init(server){
 
 
- io=new Server(server,{
-    cors:{
-        origin:"*"
-    }
- });
-
-
- createCards();
+io=new Server(server,{
+cors:{
+origin:"*"
+}
+});
 
 
 
- io.on("connection",(socket)=>{
-
-
-    socket.emit(
-        "cardsList",
-        cards
-    );
+create600Cards();
 
 
 
-    socket.on("chooseCard",(data)=>{
-
-
-        let card=cards.find(
-            c=>c.id==data.cardId
-        );
-
-
-        if(card){
-
-            socket.emit(
-                "cardSelected",
-                card
-            );
-
-        }
-
-
-    });
+io.on("connection",(socket)=>{
 
 
 
- });
+socket.emit(
+"cardsList",
+cards
+);
 
 
 
- startCountdown();
+
+
+socket.on("chooseCard",(data)=>{
+
+
+
+let card=cards.find(
+x=>x.id==data.cardId
+);
+
+
+
+if(card){
+
+
+socket.emit(
+"cardSelected",
+{
+...card,
+called:calledNumbers
+}
+);
+
 
 
 }
 
 
 
-module.exports={
- init
-};
+});
+
+
+
+
+
+});
+
+
+
+startCountdown();
+
+
+
+}
+
+
+
+
+
+module.exports={init};
