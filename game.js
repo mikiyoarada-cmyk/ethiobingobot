78fd284f1e6c2c8fa45f16b0adfe5c110afa6d2f
Mikiyoarada-cmyk <mikiyoarada@gmail.com>
2026-06-18 06:58:56 -0700

    fix real bingo 5x5 cards

--- game.js
+++ game.js
@@ -9,45 +9,120 @@ let gameInterval;
 let countdown = 30;
 
 
-/* CREATE 600 CARTELAS */
+
+/* CREATE REAL BINGO 5x5 CARD */
+function createCard(id){
+
+    let B = [];
+    let I = [];
+    let N = [];
+    let G = [];
+    let O = [];
+
+
+    while(B.length < 5){
+        let n = random(1,15);
+        if(!B.includes(n)) B.push(n);
+    }
+
+
+    while(I.length < 5){
+        let n = random(16,30);
+        if(!I.includes(n)) I.push(n);
+    }
+
+
+    while(N.length < 5){
+        let n = random(31,45);
+        if(!N.includes(n)) N.push(n);
+    }
+
+
+    while(G.length < 5){
+        let n = random(46,60);
+        if(!G.includes(n)) G.push(n);
+    }
+
+
+    while(O.length < 5){
+        let n = random(61,75);
+        if(!O.includes(n)) O.push(n);
+    }
+
+
+    return {
+
+        id:id,
+
+        numbers:[
+
+            B[0],I[0],N[0],G[0],O[0],
+            B[1],I[1],N[1],G[1],O[1],
+            B[2],I[2],"FREE",G[2],O[2],
+            B[3],I[3],N[3],G[3],O[3],
+            B[4],I[4],N[4],G[4],O[4]
+
+        ]
+
+    };
+
+}
+
+
+
+function random(min,max){
+
+ return Math.floor(Math.random()*(max-min+1))+min;
+
+}
+
+
+
+/* CREATE 600 UNIQUE CARDS */
 function createCards(){
 
-  cards = [];
+ cards=[];
 
-  for(let i = 1; i <= 600; i++){
+ let saved=[];
 
-    let numbers = [];
 
-    while(numbers.length < 25){
+ while(cards.length < 600){
 
-      let n = Math.floor(Math.random()*75)+1;
+    let card=createCard(cards.length+1);
 
-      if(!numbers.includes(n)){
-        numbers.push(n);
-      }
+
+    let key=JSON.stringify(card.numbers);
+
+
+    if(!saved.includes(key)){
+
+        saved.push(key);
+
+        cards.push(card);
 
     }
 
-    cards.push({
-      id:i,
-      numbers:numbers
-    });
-
-  }
+ }
 
 }
+
 
 
 /* COUNTDOWN */
 function startCountdown(){
 
-  countdown = 30;
+ countdown=30;
 
-  let timer = setInterval(()=>{
 
-    io.emit("countdown", {
-      countdown: countdown
-    });
+ let timer=setInterval(()=>{
+
+
+    io.emit(
+        "countdown",
+        {
+            countdown:countdown
+        }
+    );
 
 
     countdown--;
@@ -55,14 +130,14 @@ function startCountdown(){
 
     if(countdown < 0){
 
-      clearInterval(timer);
+        clearInterval(timer);
 
-      startGame();
+        startGame();
 
     }
 
 
-  },1000);
+ },1000);
 
 }
 
@@ -71,50 +146,58 @@ function startCountdown(){
 /* START GAME */
 function startGame(){
 
-  calledNumbers=[];
-
-  io.emit("gameStart");
+ calledNumbers=[];
 
 
-  gameInterval=setInterval(()=>{
+ io.emit("gameStart");
+
+
+ gameInterval=setInterval(()=>{
 
     callNumber();
 
-  },3000);
+ },3000);
 
 
 }
 
 
 
-/* CALL NUMBER */
+/* CALL NUMBERS */
 function callNumber(){
 
-  if(calledNumbers.length>=75){
+
+ if(calledNumbers.length>=75){
 
     endGame();
 
     return;
 
-  }
+ }
 
 
-  let n;
+ let n;
 
 
-  do{
+ do{
 
-    n=Math.floor(Math.random()*75)+1;
-
-  }while(calledNumbers.includes(n));
+    n=random(1,75);
 
 
-  calledNumbers.push(n);
+ }while(calledNumbers.includes(n));
 
 
-  io.emit("number",{
-    number:n
-  });
+
+ calledNumbers.push(n);
+
+
+
+ io.emit(
+    "number",
+    {
+        number:n
+    }
+ );
 
 
 }
@@ -132,7 +215,9 @@ function endGame(){
 
  setTimeout(()=>{
 
-   startCountdown();
+    createCards();
+
+    startCountdown();
 
  },5000);
 
@@ -145,48 +230,51 @@ function endGame(){
 function init(server){
 
 
- io = new Server(server,{
-   cors:{
-    origin:"*"
-   }
+ io=new Server(server,{
+    cors:{
+        origin:"*"
+    }
  });
 
 
  createCards();
 
 
+
  io.on("connection",(socket)=>{
 
 
-   socket.emit(
-    "cardsList",
-    cards
-   );
+    socket.emit(
+        "cardsList",
+        cards
+    );
 
 
-   socket.on("chooseCard",(data)=>{
+
+    socket.on("chooseCard",(data)=>{
 
 
-      let card = cards.find(
-        c=>c.id == data.cardId
-      );
-
-
-      if(card){
-
-        socket.emit(
-          "cardSelected",
-          card
+        let card=cards.find(
+            c=>c.id==data.cardId
         );
 
-      }
+
+        if(card){
+
+            socket.emit(
+                "cardSelected",
+                card
+            );
+
+        }
 
 
-   });
+    });
 
 
 
  });
+
 
 
  startCountdown();
