b9d45defc97f0236ca6c5814facdfd763f3e758a
Mikiyoarada-cmyk <mikiyoarada@gmail.com>
2026-06-18 05:18:08 -0700

    replace game html

--- public/client.js
+++ public/client.js
@@ -6,31 +6,84 @@ const lastEl = document.getElementById("last");
 
 let userId = Math.floor(Math.random() * 999999);
 
+/* ================= SHOW 600 CARTELAS ================= */
+
+socket.on("cardsList", (cards) => {
+
+cardEl.innerHTML = "";
+
+cards.forEach(card => {
+
+```
+const btn = document.createElement("button");
+
+btn.innerText = "Cartela #" + card.id;
+
+btn.style.margin = "4px";
+
+btn.onclick = () => {
+
+  socket.emit("chooseCard", {
+    userId,
+    cardId: card.id
+  });
+
+};
+
+cardEl.appendChild(btn);
+```
+
+});
+
+});
+
+/* ================= SELECTED CARD ================= */
+
+socket.on("cardSelected", (card) => {
+
+cardEl.innerHTML =
+"<h3>Selected Cartela #" + card.id + "</h3>" +
+card.numbers.map(n =>
+`<span class="num">${n}</span>`
+).join("");
+
+});
+
 /* ================= COUNTDOWN ================= */
-socket.on("countdown", (data) => {
-  countdownEl.innerText = "Starts in: " + data.countdown;
+
+socket.on("countdown", (seconds) => {
+countdownEl.innerText = "Starts In: " + seconds;
 });
 
 /* ================= GAME START ================= */
+
 socket.on("gameStart", () => {
-  countdownEl.innerText = "🎮 GAME STARTED";
+countdownEl.innerText = "GAME STARTED";
 });
 
-/* ================= CARD ================= */
-socket.emit("chooseCard", { userId });
+/* ================= CALLED NUMBER ================= */
 
-socket.on("cardSelected", (card) => {
-  cardEl.innerHTML = card.numbers
-    .map(n => `<span class="num">${n}</span>`)
-    .join("");
-});
-
-/* ================= NUMBER ================= */
-socket.on("number", (data) => {
-  lastEl.innerText = data.number;
+socket.on("number", (number) => {
+lastEl.innerText = number;
 });
 
 /* ================= WINNER ================= */
+
 socket.on("winner", (data) => {
-  alert("🏆 WINNER: " + data.userId);
+
+alert(
+"WINNER USER: " +
+data.userId +
+" | CARTELA: " +
+data.cardId
+);
+
+});
+
+/* ================= RESET ================= */
+
+socket.on("reset", () => {
+
+location.reload();
+
 });
